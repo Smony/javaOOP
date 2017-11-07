763d40dcd9354339dbcedbf1db4cf7b94b47f59c
Smony <dev.symonchuk@gmail.com>
2017-11-07 12:02:07 +0200

    add enter key

--- day00/src/Main.java
+++ day00/src/Main.java
@@ -1,5 +1,7 @@
 import classes.Cat;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -27,5 +29,21 @@ public class Main {
 //        catTwo.sleep(); //meow or sleeping
         System.out.println(catTwo.say());
         System.out.println(catTwo.toString());
+
+        System.out.println("-----------------------------------------------------------");
+        Cat catThree = new Cat();
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите Имя Вашего Котенка:");
+        catThree.setName(in.next());
+        System.out.print("Введите Цвет:)");
+        catThree.setColor(in.next());
+        System.out.print("Введите Возраст:");
+        catThree.setAge(in.nextInt());
+
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Name: " + catThree.getName());
+        System.out.println("Color: " + catThree.getColor());
+        System.out.println("Age: " + catThree.getAge());
     }
 }
